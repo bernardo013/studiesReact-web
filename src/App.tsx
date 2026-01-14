470ce651fe21715c21a3d186b0d3a727aca0f178
bernardo013 <105383046+bernardo013@users.noreply.github.com>
2026-01-14 09:34:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Botao from "./components/Button";
+function App() { 
+
+  return (
+    <div>
+      <Botao />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const botao = () => {
+  return (
+    <button>
+        Test
+    </button>
+  )
+}
+
+export default botao
